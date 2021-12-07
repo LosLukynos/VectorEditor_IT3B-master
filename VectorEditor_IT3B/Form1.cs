a27774875973ca165f5c59356941cd49bdcf7110
Lukáš Macháček <Machacek.Lukas@stud.vassboskovice.cz>
2021-12-07 13:33:00 +0100

    Přidejte soubory projektu.

---
+++ VectorEditor_IT3B/Form1.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Newtonsoft.Json;
+
+namespace VectorEditor_IT3B
+{
+    public partial class Form1 : Form
+    {
+        Shapes selectedShapes = Shapes.None;
+
+        List<Point> shapes;
+        System.Drawing.Point mousePoint;
+        Color DefafaultButtonColor;
+        public Form1()
+        {   
+            InitializeComponent();
+            shapes = new List<Point>();
+            DefafaultButtonColor = btnLine.BackColor;
+        }
+
+        private void pboxCanvas_MouseMove(object sender, MouseEventArgs e)
+        {
+            this.Text = e.Location.ToString();
+            mousePoint = e.Location;
+            //pboxCanvas.Refresh();
+        }
+
+        private void pboxCanvas_MouseClick(object sender, MouseEventArgs e)
+        {
+            shapes.Add(new Point(e.Location.X,e.Location.Y));
+            pboxCanvas.Refresh();
+        }
+
+        private void pboxCanvas_Paint(object sender, PaintEventArgs e)
+        {
+            foreach (var shape in shapes)
+            {
+                shape.Draw(e.Graphics);
+            }
+        }
+        private void sfd_FileOk(object sender, CancelEventArgs e)
+        {
+            var json = JsonConvert.SerializeObject(shapes/*, Formatting.Indented*/);
+            File.WriteAllText(sfd.FileName, json);
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if(e.KeyCode == Keys.S && e.Control)
+            {
+                sfd.ShowDialog();
+            }
+            else if (e.KeyCode == Keys.O && e.Control)
+            {
+                ofd.ShowDialog();
+            }
+        }
+
+        private void ofd_FileOk(object sender, CancelEventArgs e)
+        {
+            try
+            {
+                var json = File.ReadAllText(ofd.FileName);
+                shapes = JsonConvert.DeserializeObject<List<Point>>(json);
+                pboxCanvas.Refresh();
+            }
+            catch
+            {
+                MessageBox.Show("Nepodařilo se otevřít.");
+            }
+        }
+
+        private void btnPoint_Click(object sender, EventArgs e)
+        {
+            selectedShapes = Shapes.Point;
+            btnPoint.BackColor = Color.LightBlue;
+            btnLine.BackColor = Color.FromKnownColor(KnownColor.Control);
+        }
+
+        private void btnLine_Click(object sender, EventArgs e)
+        {
+            selectedShapes = Shapes.Line;
+            btnLine.BackColor = Color.LightBlue;
+            btnPoint.BackColor = Color.FromKnownColor(KnownColor.Control);
+        }
+    }
+}
